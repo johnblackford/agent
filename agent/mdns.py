446ab254f009c2b9dff166bedfe1e317b2e37e25
John Blackford <john.blackford@gmail.com>
2018-08-08 13:07:06 -0500

    Add in mDNS Listening function for USP Controllers

--- agent/mdns.py
+++ agent/mdns.py
@@ -33,6 +33,7 @@
 
 
 import socket
+import logging
 import zeroconf
 
 
@@ -47,7 +48,6 @@ class Announcer(object):
         self._instance = usp_endpoint_id
         self._service = "_usp-agt-coap._udp."
         self._domain = "local."
-
 
     def announce(self, friendly_name=None, subtypes=None):
         """Announce myself on mDNS by registering a service"""
@@ -71,3 +71,90 @@ class Announcer(object):
     def cleanup(self):
         """Clean up the ZeroConf object"""
         self._zconf.close()
+
+
+class Listener(object):
+    """Listen for CoAP Controllers via mDNS"""
+    def __init__(self):
+        """Initialize the Listener Class"""
+        self._zconf = None
+        self._browser = None
+        self._host_name_to_ip_map = {}
+        self._endpoint_id_to_url_map = {}
+        self._service = "_usp-ctl-coap._udp.local."
+        self._logger = logging.getLogger(self.__class__.__name__)
+
+    def listen(self):
+        """Listen for mDNS registrations against a specific service"""
+        self._zconf = zeroconf.Zeroconf(interfaces=zeroconf.InterfaceChoice.Default)
+        self._browser = zeroconf.ServiceBrowser(self._zconf, self._service, self)
+
+    def add_service(self, zconf, svc_type, name):
+        """Process an incoming mDNS Service Registration"""
+        info = zconf.get_service_info(svc_type, name)
+        self._logger.info("Processing an mDNS Service Registration for [%s]: %s", name, info)
+
+        controller_coap_url = self._get_coap_url(info)
+        controller_endpoint_id = self._get_endpoint_id(name)
+        self._logger.info("mDNS Service Registration for [%s] resolved to Endpoint ID [%s] and CoAP URL [%s]",
+                          name, controller_endpoint_id, controller_coap_url)
+
+        if controller_endpoint_id in self._endpoint_id_to_url_map:
+            self._logger.info("mDNS Service Registration for [%s] updated the URL for Endpoint ID [%s] to: %s",
+                              name, controller_endpoint_id, controller_coap_url)
+        else:
+            self._logger.info("mDNS Service Registration for [%s] created an entry for Endpoint ID [%s] with: %s",
+                              name, controller_endpoint_id, controller_coap_url)
+        self._endpoint_id_to_url_map[controller_endpoint_id] = controller_coap_url
+        self._host_name_to_ip_map[name] = socket.inet_ntoa(info.address)
+
+    def remove_service(self, zconf, svc_type, name):
+        """Process an incoming mDNS Service De-Registration"""
+        self._logger.debug("Service [%s] was Removed", name)
+        controller_endpoint_id = self._get_endpoint_id(name)
+        self._logger.info("mDNS Service De-Registration caused Endpoint ID [%s] to be removed",
+                          controller_endpoint_id)
+        del self._endpoint_id_to_url_map[controller_endpoint_id]
+        del self._host_name_to_ip_map[name]
+
+    def resolve_addr(self, endpoint_id):
+        """Retrieve the current CoAP URL for a given USP Endpoint ID"""
+        url = None
+
+        if endpoint_id in self._endpoint_id_to_url_map:
+            url = self._endpoint_id_to_url_map[endpoint_id]
+
+        return url
+
+    def resolve_host(self, host_name):
+        """Retrieve the IP Address for the provided Host Name"""
+        ip_addr = None
+
+        if host_name in self._host_name_to_ip_map:
+            ip_addr = self._host_name_to_ip_map[host_name]
+
+        return ip_addr
+
+    def cleanup(self):
+        """Clean up the ZeroConf object"""
+        self._browser.cancel()
+        self._zconf.close()
+
+    def _get_coap_url(self, info):
+        """Build a CoAP URL for the Controller based on the Service Info provided"""
+        coap_url = None
+        port = str(info.port)
+        addr = socket.inet_ntoa(info.address)
+        resource_path = info.properties.get(b'path')
+
+        if resource_path is not None:
+            decoded_resource_path = resource_path.decode('ascii')
+            coap_url = "coap://" + addr + ":" + port + "/" + decoded_resource_path
+        else:
+            self._logger.warning("Discovered a malformed CoAP Controller - No 'path' Property")
+
+        return coap_url
+
+    def _get_endpoint_id(self, name):
+        """Retrieve the USP Endpoint ID from the mDNS Name"""
+        return name.split(".")[0]
